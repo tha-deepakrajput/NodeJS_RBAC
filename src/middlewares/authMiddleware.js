1d003ffaf5de03acc9b313a5ef343eefe07f6597
tha-deepakrajput <thadeepakrajput@Deepaks-MacBook-Air.local>
2025-06-20 19:09:03 +0530

    role based access control middleware has been implemented in the userRoutes

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,16 +1,28 @@
 const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
-    let token; 
+    let token;
     let authHeader = req.headers.Authorization || req.headers.authorization;
 
     if (authHeader && authHeader.startsWith("Bearer")) {
         token = authHeader.split(" ")[1];
 
         if (!token) {
-            return res.status(401).json({message: "No token, Authorization Denied"});
+            return res.status(401).json({ message: "No token, Authorization Denied" });
         }
 
-        
+        try {
+            const decode = jwt.verify(token, process.env.JWT_SECRET);
+            req.user = decode;
+            console.log("The decoded user is : ", req.user);
+            next();
+        } catch (err) {
+            res.status(400).json({ message: "Token is not valid" });
+        }
+    }
+    else {
+        return res.status(401).json({ message: "No token, Authorization Denied" });
     }
 }
+
+module.exports = verifyToken;
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,19 +1,22 @@
 const express = require("express");
 
+const verifyToken = require("../middlewares/authMiddleware");
+const authorizeRoles = require("../middlewares/roleMiddleware");
+
 const router = express.Router();
 
 // For Admin: 
-router.get("/admin", (req, res) => {
+router.get("/admin", verifyToken, authorizeRoles("admin"), (req, res) => {
     res.status(200).json({message: "Welcome Admin"});
 });
 
 // For Manager: 
-router.get("/manager", (req, res) => {
+router.get("/manager", verifyToken, authorizeRoles("admin", "manager"), (req, res) => {
     res.status(200).json({message: "Welcome Manager"});
 });
 
 // For User: 
-router.get("/user", (req, res) => {
+router.get("/user", verifyToken, authorizeRoles("admin", "manager", "user"), (req, res) => {
     res.status(200).json({message: "Welcome User"});
 });
 
